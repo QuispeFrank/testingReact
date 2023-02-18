110d0b6ba92c7406631ca6dfcb88ca7ab6b4adf4
FranQ <frankchristopherquispe@gmail.com>
2023-02-18 15:35:59 -0500

    Update url prefix for cohorts_api blueprint

--- app/v1/__init__.py
+++ app/v1/__init__.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 from .routes.cohorts import cohorts_api
 
 
 def v1_attach_apis(app):
-    app.register_blueprint(cohorts_api, url_prefix="/v1/cohorts")
+    app.register_blueprint(cohorts_api, url_prefix="/api/v1/cohort")
     return app
